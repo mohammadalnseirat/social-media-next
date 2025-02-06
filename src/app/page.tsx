2d1898a7640b5b525ead2998f221cf0797ad421d
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2025-02-07 03:28:44 +0400

    Add Dark/Light Mode Functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
+import ModeToggle from "@/components/ModeToggle";
+import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       <SignedOut>
-        <SignInButton mode="modal" />
+        <SignInButton mode="modal">
+          <Button>Sign In</Button>
+        </SignInButton>
       </SignedOut>
       <SignedIn>
         <UserButton />
       </SignedIn>
+      <ModeToggle />
     </div>
   );
 }
